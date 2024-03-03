f24e3c2a99db6644910005980359bbac7d1d1989
khalid544 <khalidelwalid544@gmail.com>
2024-03-03 17:49:18 +0200

    add

--- script (1).js
+++ script (1).js
@@ -1,28 +1,14 @@
 // Wrap the entire contents of script.js inside of an IIFE
 (function () {
 
-  var helloSpeaker = {};
-  helloSpeaker.speak = function(name) {
-    console.log("Hello " + name);
-  };
-
-  var byeSpeaker = {};
-  byeSpeaker.speak = function(name) {
-    console.log("Good Bye " + name);
-  };
-
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-  // Loop over the names array and say either 'Hello' or "Good Bye"
   for (var i = 0; i < names.length; i++) {
-    // Retrieve the first letter of the current name in the loop.
     var firstLetter = names[i].charAt(0).toLowerCase();
-
-    // Compare the 'firstLetter' retrieved to 'j'.
     if (firstLetter === 'j') {
-      byeSpeaker.speak(names[i]);
+      console.log("Good Bye " + names[i]);
     } else {
-      helloSpeaker.speak(names[i]);
+      console.log("Hello " + names[i]);
     }
   }
 
